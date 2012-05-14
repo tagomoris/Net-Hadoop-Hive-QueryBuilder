d42d3e1d5a0c5313440022de6d948dd0fd8ad90b
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-05-14 17:55:00 +0900

    add aggregate functions

--- lib/Net/Hadoop/Hive/QueryBuilder/Functions/Aggregate.pm
+++ lib/Net/Hadoop/Hive/QueryBuilder/Functions/Aggregate.pm
@@ -16,6 +16,25 @@ use Data::SExpression::Symbol;
 sub functions {
     +[
         { type => 'f', name => 'count', proc => \&count },
+        { type => 'f', name => 'sum', proc => \&sum },
+        { type => 'f', name => 'avg', proc => \&avg },
+        { type => 'f', name => 'min', proc => \&min },
+        { type => 'f', name => 'max', proc => \&max },
+
+        # { type => 'f', name => 'variance', proc => \&variance },
+        # { type => 'f', name => 'var_samp', proc => \&var_samp },
+        # { type => 'f', name => 'stddev_pop', proc => \&stddev_pop },
+        # { type => 'f', name => 'stddev_samp', proc => \&stddev_samp },
+        # { type => 'f', name => 'covar_pop', proc => \&covar_pop },
+        # { type => 'f', name => 'covar_samp', proc => \&covar_samp },
+        # { type => 'f', name => 'corr', proc => \&corr },
+
+        { type => 'f', name => 'percentile', proc => \&percentile },
+        { type => 'f', name => 'percentile_approx', proc => \&percentile_approx },
+
+#        { type => 'f', name => 'histogram_numeric', proc => \&histogram_numeric },
+
+        { type => 'f', name => 'collect_set', proc => \&collect_set },
     ]
 }
 
@@ -54,15 +73,49 @@ sub count {
 }
 
 sub sum {
+    # (sum (field col1))
+    # (sum distinct (field col1))
+    my ($b,@args) = @_;
+    if (scalar(@args) < 1 or scalar(@args) > 2) {
+        die "sum nees 1 column or 2 arguments ('distinct' + col)";
+    }
+    if (scalar(@args) == 1) {
+        return 'SUM(' . $b->produce_value($args[0]) . ')';
+    }
+    my $distinct = shift @args;
+    unless (ref($distinct) eq 'Data::SExpression::Symbol' and $distinct->name eq 'distinct') {
+        die "sum with 2 arguments needs 'distinct' for 1st argument";
+    }
+    'SUM(DISTINCT ' . $b->produce_value($args[0]) . ')';
 }
 
 sub avg {
+    # (avg (field col1))
+    # (avg distinct (field col1))
+    my ($b,@args) = @_;
+    if (scalar(@args) < 1 or scalar(@args) > 2) {
+        die "avg nees 1 column or 2 arguments ('distinct' + col)";
+    }
+    if (scalar(@args) == 1) {
+        return 'AVG(' . $b->produce_value($args[0]) . ')';
+    }
+    my $distinct = shift @args;
+    unless (ref($distinct) eq 'Data::SExpression::Symbol' and $distinct->name eq 'distinct') {
+        die "avg with 2 arguments needs 'distinct' for 1st argument";
+    }
+    'AVG(DISTINCT ' . $b->produce_value($args[0]) . ')';
 }
 
 sub min {
+    my ($b,@args) = @_;
+    if (scalar(@args) != 1) { die "(min (field col1))"; }
+    'MIN(' . $b->produce_value($args[0]) . ')';
 }
 
 sub max {
+    my ($b,@args) = @_;
+    if (scalar(@args) != 1) { die "(max (field col1))"; }
+    'MAX(' . $b->produce_value($args[0]) . ')';
 }
 
 sub variance {} #TODO
@@ -74,14 +127,27 @@ sub covar_samp {} #TODO
 sub corr {} #TODO
 
 sub percentile {
+    my ($b,@args) = @_;
+    if (scalar(@args) != 2) {
+        die "(percentile (field col1) (number 0.95)) or (percentile (field col1) (array 0.9 0.91 0.92 ...))";
+    }
+    'PERCENTILE(' . $b->produce_value($args[0]) . ', ' . $b->produce_value($args[1]) . ')';
 }
 
 sub percentile_approx {
+    my ($b,@args) = @_;
+    if (scalar(@args) != 2 and scalar(@args) != 3) {
+        die "(percentile_approx (field col1) (p) [(number B)])";
+    }
+    'PERCENTILE_APPROX(' . join(', ', map { $b->produce_value($_) } @args) . ')';
 }
 
 sub histogram_numeric {} #TODO
 
 sub collect_set {
+    my ($b,@args) = @_;
+    if (scalar(@args) != 1) { die "(collect_set (field col))"; }
+    'COLLECT_SET(' . $b->produce_value($args[0]) . ')';
 }
 
 1;
--- t/Function_Aggreagate.t
+++ t/Function_Aggreagate.t
@@ -28,4 +28,86 @@ subtest 'count' => sub {
     is (pp('count')->($bb, ss('distinct'), [ss('field'), ss('col1')], [ss('field'), ss('col2')]), "COUNT(DISTINCT col1, col2)");
 };
 
+subtest 'sum' => sub {
+    is (pp('sum')->($bb, [ss('field'), ss('col1')]), "SUM(col1)");
+    is (pp('sum')->($bb, ss('distinct'), [ss('field'), ss('col1')]), "SUM(DISTINCT col1)");
+};
+
+subtest 'avg' => sub {
+    is (pp('avg')->($bb, [ss('field'), ss('col1')]), "AVG(col1)");
+    is (pp('avg')->($bb, ss('distinct'), [ss('field'), ss('col1')]), "AVG(DISTINCT col1)");
+};
+
+subtest 'min' => sub {
+    is (pp('min')->($bb, [ss('field'), ss('col1')]), "MIN(col1)");
+};
+
+subtest 'max' => sub {
+    is (pp('max')->($bb, [ss('field'), ss('col1')]), "MAX(col1)");
+};
+
+subtest 'var_pop' => sub {
+    is (1,1);
+};
+
+subtest 'var_samp' => sub {
+    is (1,1);
+};
+
+subtest 'stddev_pop' => sub {
+    is (1,1);
+};
+
+subtest 'stddev_samp' => sub {
+    is (1,1);
+};
+
+subtest 'covar_pop' => sub {
+    is (1,1);
+};
+
+subtest 'covar_samp' => sub {
+    is (1,1);
+};
+
+subtest 'corr' => sub {
+    is (1,1);
+};
+
+push @defset, { type => 'f', name => 'array_construct', proc => pp('array_construct') };
+$bb = bb(@defset);
+
+subtest 'percentile' => sub {
+    is (pp('percentile')->($bb, [ss('field'), ss('col1')], [ss('number'), ss('0.95')]),
+        "PERCENTILE(col1, 0.95)");
+    is (pp('percentile')->($bb,
+                           [ss('field'), ss('col1')],
+                           [ss('array_construct'), [ss('number'), ss('0.90')], [ss('number'), ss('0.95')]]),
+        "PERCENTILE(col1, array(0.90, 0.95))");
+};
+
+subtest 'percentile_approx' => sub {
+    is (pp('percentile_approx')->($bb, [ss('field'), ss('col1')], [ss('number'), ss('0.95')]),
+        "PERCENTILE_APPROX(col1, 0.95)");
+    is (pp('percentile_approx')->($bb, [ss('field'), ss('col1')], [ss('number'), ss('0.95')], [ss('number'), ss('50000')]),
+        "PERCENTILE_APPROX(col1, 0.95, 50000)");
+    is (pp('percentile_approx')->($bb,
+                                  [ss('field'), ss('col1')],
+                                  [ss('array_construct'), [ss('number'), ss('0.90')], [ss('number'), ss('0.95')]]),
+        "PERCENTILE_APPROX(col1, array(0.90, 0.95))");
+    is (pp('percentile_approx')->($bb,
+                                  [ss('field'), ss('col1')],
+                                  [ss('array_construct'), [ss('number'), ss('0.90')], [ss('number'), ss('0.95')]],
+                                  [ss('number'), ss('3000')]),
+        "PERCENTILE_APPROX(col1, array(0.90, 0.95), 3000)");
+};
+
+subtest 'histogram_numeric' => sub {
+    is (1,1);
+};
+
+subtest 'collect_set' => sub {
+    is (pp('collect_set')->($bb, [ss('field'), ss('col1')]), "COLLECT_SET(col1)");
+};
+
 done_testing();
